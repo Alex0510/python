a000bb226fbab181b5084a4b06b0385166ec873c
Mr.Eric <Alex0510@users.noreply.github.com>
2026-04-15 19:20:07 +0800

    更新 ACFloating.m

--- ACFloating.m
+++ ACFloating.m
@@ -2,32 +2,50 @@
 #import "ACManager.h"
 
 @interface ACFloating : UIWindow
+@property (nonatomic, strong) UIButton *btn;
 @end
 
 @implementation ACFloating
 
 - (instancetype)init {
+
     self = [super initWithFrame:CGRectMake(100, 200, 70, 70)];
 
-    self.windowLevel = UIWindowLevelAlert + 10;
-    self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
-    self.layer.cornerRadius = 35;
-    self.hidden = NO;
+    if (self) {
 
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = self.bounds;
+        // ✅ 关键1：必须有 rootVC
+        UIViewController *vc = [UIViewController new];
+        self.rootViewController = vc;
 
-    [btn setTitle:@"Clean" forState:UIControlStateNormal];
-    [btn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+        // ✅ 关键2：windowLevel 提高
+        self.windowLevel = UIWindowLevelAlert + 100;
 
-    [btn addTarget:self action:@selector(run) forControlEvents:UIControlEventTouchUpInside];
+        // UI
+        self.backgroundColor = [UIColor clearColor];
 
-    [self addSubview:btn];
+        self.btn = [UIButton buttonWithType:UIButtonTypeSystem];
+        self.btn.frame = CGRectMake(0, 0, 70, 70);
+        self.btn.layer.cornerRadius = 35;
+        self.btn.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
+
+        [self.btn setTitle:@"Clean" forState:UIControlStateNormal];
+        [self.btn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+
+        [self.btn addTarget:self action:@selector(run) forControlEvents:UIControlEventTouchUpInside];
+
+        [vc.view addSubview:self.btn];
+
+        // ✅ 关键3：必须显示
+        [self makeKeyAndVisible];
+    }
 
     return self;
 }
 
 - (void)run {
+
+    NSLog(@"[AllClean] 按钮点击");
+
     dispatch_async(dispatch_get_main_queue(), ^{
         [[ACManager shared] runFullClean];
     });
