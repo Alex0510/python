3e8a1ffdb33afd108abce480d28c7fd786458c2d
Mr.Eric <Alex0510@users.noreply.github.com>
2026-04-15 19:03:21 +0800

    更新 NDManager.m

--- NDManager.m
+++ NDManager.m
@@ -1,62 +1,113 @@
-#import "NDManager.h"
+#import "ACManager.h"
 #import <Security/Security.h>
+#import <WebKit/WebKit.h>
 
-@implementation NDManager
+@implementation ACManager
 
 + (instancetype)shared {
-    static NDManager *m;
+    static ACManager *m;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        m = [[NDManager alloc] init];
+        m = [ACManager new];
     });
     return m;
 }
 
-- (void)cleanSandbox {
-    NSString *home = NSHomeDirectory();
+#pragma mark - 当前 App
 
-    NSArray *paths = @[
-        @"Documents",
-        @"Library/Caches",
-        @"Library/Application Support",
-        @"tmp"
-    ];
-
-    for (NSString *p in paths) {
-        NSString *full = [home stringByAppendingPathComponent:p];
-        [[NSFileManager defaultManager] removeItemAtPath:full error:nil];
-        [[NSFileManager defaultManager] createDirectoryAtPath:full
-                                  withIntermediateDirectories:YES
-                                                   attributes:nil
-                                                        error:nil];
-    }
-
-    NSLog(@"[ND] 沙盒清理完成");
+- (NSString *)currentBundle {
+    return [[NSBundle mainBundle] bundleIdentifier];
 }
 
-- (void)cleanKeychain {
-    NSArray *secClasses = @[
+#pragma mark - Keychain 清理（调试用途）
+
+- (void)clearKeychain {
+    NSArray *classes = @[
         (__bridge id)kSecClassGenericPassword,
         (__bridge id)kSecClassInternetPassword,
-        (__bridge id)kSecClassCertificate,
         (__bridge id)kSecClassKey,
         (__bridge id)kSecClassIdentity
     ];
 
-    for (id secClass in secClasses) {
-        NSDictionary *query = @{(__bridge id)kSecClass: secClass};
+    for (id cls in classes) {
+        NSDictionary *query = @{(__bridge id)kSecClass: cls};
         SecItemDelete((__bridge CFDictionaryRef)query);
     }
 
-    NSLog(@"[ND] Keychain 清理完成");
+    NSLog(@"[AllClean] Keychain cleared");
 }
 
-- (void)resetUserDefaults {
-    NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
-    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:bundleID];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+#pragma mark - 沙盒清理
 
-    NSLog(@"[ND] UserDefaults 已重置");
+- (void)clearSandbox {
+
+    NSString *home = NSHomeDirectory();
+    NSFileManager *fm = [NSFileManager defaultManager];
+
+    NSArray *dirs = @[@"Documents", @"Library", @"tmp"];
+
+    for (NSString *dir in dirs) {
+        NSString *path = [home stringByAppendingPathComponent:dir];
+        NSArray *files = [fm contentsOfDirectoryAtPath:path error:nil];
+
+        for (NSString *file in files) {
+            NSString *full = [path stringByAppendingPathComponent:file];
+            [fm removeItemAtPath:full error:nil];
+        }
+    }
+
+    NSLog(@"[AllClean] Sandbox cleared");
+}
+
+#pragma mark - Preferences
+
+- (void)clearPreferences {
+
+    NSString *bundle = [self currentBundle];
+
+    NSString *pref = [NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", bundle];
+
+    [[NSFileManager defaultManager] removeItemAtPath:pref error:nil];
+
+    NSLog(@"[AllClean] Preferences cleared");
+}
+
+#pragma mark - Web 数据
+
+- (void)clearWeb {
+
+    NSSet *types = [WKWebsiteDataStore allWebsiteDataTypes];
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:0];
+
+    [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:types
+                                              modifiedSince:date
+                                          completionHandler:^{
+        NSLog(@"[AllClean] Web data cleared");
+    }];
+}
+
+#pragma mark - URLCache
+
+- (void)clearCache {
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    NSLog(@"[AllClean] URLCache cleared");
+}
+
+#pragma mark - 主入口
+
+- (void)runFullClean {
+
+    NSLog(@"======== AllClean START ========");
+
+    [self clearKeychain];
+    [self clearSandbox];
+    [self clearPreferences];
+    [self clearWeb];
+    [self clearCache];
+
+    NSLog(@"======== AllClean DONE ========");
+
+    exit(0);
 }
 
 @end
