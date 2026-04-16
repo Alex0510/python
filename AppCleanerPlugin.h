41916c41023e10a0870a1c125a964a54b5e7a698
Mr.Eric <Alex0510@users.noreply.github.com>
2026-04-16 11:13:28 +0800

    更新 AppCleanerPlugin.h

--- AppCleanerPlugin.h
+++ AppCleanerPlugin.h
@@ -11,29 +11,16 @@ NS_ASSUME_NONNULL_BEGIN
 @interface AppCleanerPlugin : NSObject
 
 + (instancetype)sharedPlugin;
-
-/// 显示悬浮清理按钮
 - (void)showFloatingButton;
-
-/// 隐藏悬浮清理按钮
 - (void)hideFloatingButton;
 
-/// 清理当前App的所有数据
+// 清理方法
+- (void)quickClean;
 - (void)cleanAllData;
-
-/// 清理沙盒 (Documents/Caches/tmp)
 - (void)cleanSandbox;
-
-/// 清理 NSUserDefaults
 - (void)cleanUserDefaults;
-
-/// 清理 Cookie 和 WebKit
 - (void)cleanCookiesAndWebKit;
-
-/// 清理 Keychain
 - (void)cleanKeychain;
-
-/// 退出App
 - (void)exitApp;
 
 @end
